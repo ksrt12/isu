bbe20582377f3d3d4336f001987c87f4a23ebc99
Kazakov Stepan <ksrt12group@gmail.com>
2020-08-29 01:17:00 +0300

    fix del exam

--- de.user.js
+++ de.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Del exam
 // @date         2020-08-29
-// @version      1.2
+// @version      1.2.1
 // @author       kazakovstepan
 // @namespace    ITMO University
 // @description  Make описи great again!
@@ -14,12 +14,12 @@
 // @run-at       document-start
 // ==/UserScript==
 
-var td = document.querySelector("tbody > tr:nth-child(2) > td");
+let td = document.querySelector("tbody > tr:nth-child(2) > td");
 if (td.innerText.includes("Расписка о получении")) {
 	for (let i = 19; i > 10; i--) {
 		document.querySelector("tbody > tr:nth-child(" + i + ")").remove();
 	}
 	document.querySelector("tbody > tr:nth-child(9) > td").innerText = "29 августа 2020 г.";
 	document.querySelector("tbody > tr:nth-child(7)").remove();
-	td.innerText = td.replace("Расписка о получении документов", "Опись личного дела");
+	td.innerText = td.innerText.replace("Расписка о получении документов", "Опись личного дела");
 }
