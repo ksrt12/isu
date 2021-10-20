36f0b64c2205f09956c8ba99ce5f51ef8a5a78a5
Stepan Kazakov <ksrt12group@gmail.com>
2021-10-20 04:32:32 +0300

    Update from 89660c2: userscripts: gto: fix comments deletion

--- scripts/gto.user.js
+++ scripts/gto.user.js
@@ -1,4 +1,20 @@
 "use strict";
+// ==UserScript==
+// @name        ГТО
+// @version     1.1
+// @date        2021-09-01
+// @author      kazakovstepan
+// @namespace   ITMO University
+// @description Let's do it
+// @homepage    https://vk.com/kazakovstepan
+// @icon        https://isu.ksrt12.ru/favicon.ico
+// @updateURL   https://isu.ksrt12.ru/scripts/gto.user.js
+// @downloadURL https://isu.ksrt12.ru/scripts/gto.user.js
+// @include     https://www.gto.ru/sign/*LN*
+// @exclude     https://www.gto.ru/sign/check#result
+// @run-at      document-end
+// @grant       none
+// ==/UserScript==
 function load_params() {
     let params = decodeURIComponent(window.location.search.replace('?', '')).split('&').reduce((p, e) => {
         const [key, val] = e.split('=');
